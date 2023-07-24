8b7ce376db41e6e093aec864aeab3a578594f45f
Xhapa <emicb0111@gmail.com>
2023-07-24 12:01:27 -0500

    Text tokenizer an tagger model cretated for EN and ES, saving model to .dill

--- src/main.py
+++ src/main.py
@@ -10,17 +10,17 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
-templates = Jinja2Templates(directory='templates')
+app.mount("/assets", StaticFiles(directory="../assets"), name="assets")
+templates = Jinja2Templates(directory='../templates')
 
 @app.get('/', response_class=HTMLResponse)
 def main(request: Request):
     return templates.TemplateResponse('home.html', {'request': request})
 
 @app.get('/text', response_class=HTMLResponse)
-def main(request: Request):
+def from_text(request: Request):
     return templates.TemplateResponse('from_text.html', {'request': request})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0",
-    port=int(os.environ.get("PORT", 8000)))
+    port=int(os.environ.get("PORT", 8000)), reload=True)
